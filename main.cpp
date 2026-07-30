e05f2f12114ee83cb137bad05f6111bd125e5802
k5nnedy <ke83062p@pace.edu>
2026-07-30 01:43:49 -0400

    Rendered sphere

--- main.cpp
+++ main.cpp
@@ -4,7 +4,18 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b= -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return discriminant >= 0;
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1,0,0);
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
@@ -28,7 +39,7 @@ int main() {
     auto camera_center = point3(0, 0, 0);
 
     // Calculate vectors across horizontal and down the vertical viewport edges
-    auto viewport_u = vec3(viewport_width, 0, 0);  // Vu Vector --> R to L
+    auto viewport_u = vec3(viewport_width, 0, 0);  // Vu Vector --> L to R
     auto viewport_v = vec3(0, -viewport_height, 0); // Vv Vector --> Top to bottom
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel
