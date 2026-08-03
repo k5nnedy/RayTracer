30473b23fe4dce8a62b17ca253f59ca43dcdc8df
k5nnedy <ke83062p@pace.edu>
2026-08-03 12:45:30 -0400

    metal material using reflection

--- material.h
+++ material.h
@@ -34,4 +34,21 @@ private:
     color albedo;
 };
 
+class metal : public material {
+public:
+    metal(const color& albedo) : albedo(albedo) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attentuation, ray& scattered)
+    const override {
+        vec3 reflected = reflect(r_in.direction(), rec.normal);
+        scattered = ray(rec.p, reflected);
+        attentuation = albedo;
+        return true;
+    }
+
+private:
+    color albedo;
+
+};
+
 #endif
